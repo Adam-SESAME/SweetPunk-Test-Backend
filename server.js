869cac461ab00b0a6ca59277d37059ab5cdb3e63
Adam-SESAME <adam.simon@sesame-digital.com>
2024-06-25 10:55:34 +0200

    :sparkles: modified cors

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import cors
 
 const app = express();
 const port = 3001;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'your-username',
-  password: 'your-password',
-  database: 'CitationsDB'
+  host: 'sql7.freesqldatabase.com',
+  user: 'sql7715824',
+  password: 'V6irYfJtgg',
+  database: 'sql7715824'
 });
 
 app.get('/citations', (req, res) => {
-  pool.query('SELECT * FROM CITATIONS', (error, results) => {
+  pool.query('SELECT * FROM citations', (error, results) => {
     if (error) throw error;
     res.send(results);
   });
